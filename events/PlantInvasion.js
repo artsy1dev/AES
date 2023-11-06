93af042cfbf0ee73c4f9ad891d28f9bff21b1def
Ainara <ainaradev7@gmail.com>
2023-11-06 18:32:38 +0100

    More changes to plant invasion

--- events/PlantInvasion.js
+++ events/PlantInvasion.js
@@ -1,12 +1,14 @@
 import { wearBind, breakRandomClothes } from "../utils/utilsCharacter";
 import { getRandomInt } from "../utils/utils";
+import { ChatRoomSendLocal, ChatRoomActionMessage } from "../utils/utilsClub";
 
 let eventInterval;
 let level = 0;
 const greenColor = "#56AB56";
 
 export function initEvent() {
-    console.log("AES: A plant invasion has started")
+    console.log("AES: A plant invasion has started");
+    ChatRoomSendLocal("A plant invasion has started");
     eventInterval = setInterval(function () {
         stepEvent();
     }, 1*1000);
@@ -35,17 +37,44 @@ function process() {
             break;
         case 1:
             wearBind(Player, "HempRope", "ItemLegs", greenColor, 1);
+            ChatRoomSendLocal("The plant wraps around your legs, twisting and tightening its grip.");
             break;
         case 2:
             wearBind(Player, "HempRope", "ItemArms", greenColor, 1);
+            ChatRoomSendLocal("The vines slithered up your arms, coiling and constricting as they entangled you.");
             break;
         case 3:
+            ChatRoomSendLocal("The plant's sinuous tendrils coiled around your thighs, slowly constricting and pulling you deeper into their verdant embrace.");
             break;
         default:
             break;
     }
     if (level > 10) {
-        breakRandomClothes(Player);
+        let clothesString = getRandomInt(5);
+        switch (clothesString) {
+            case 0:
+                ChatRoomSendLocal("With a relentless determination, the voracious plants worked their way through your clothing, tearing and ripping as they sought a path to ensnare your vulnerable flesh.");
+                break;
+            case 1:
+                ChatRoomSendLocal("The plant's tendrils, like miniature saws, sliced through your attire, leaving tattered shreds in their wake as they made their way towards your skin.");
+                break;
+            case 2:
+                ChatRoomSendLocal("As the plant invasion intensified, your clothes became a casualty, shredded by the relentless force of nature's encroachment.");
+                break;
+            case 3:
+                ChatRoomSendLocal("The invasive plants, fueled by an insatiable hunger, tore at your garments, exposing your skin to their invasive advance.");
+                break;
+            case 4:
+                ChatRoomSendLocal("Inexorably, the plant's tendrils shredded your clothing, revealing the helplessness of your situation as they reached for you with unyielding determination.");
+                break;
+            default:
+                break;
+        }
+        if (!CharacterIsNaked(Player)) {
+            breakRandomClothes(Player);
+        } else {
+
+        }
     }
     if (level > 250) {
         stopEvent();
--- utils/utilsClub.js
+++ utils/utilsClub.js
@@ -1,10 +1,49 @@
-const modVersion = "0.0.1"
-const modUrl = 'https://ainaradev.github.io/AES/AES.js'
-const modRepository = 'https://github.com/ainaradev7/ainaradev7.github.ios'
 
 export function InfoBeep(msg, timer = 3000) {
 	ServerBeep = {
 		Timer: CommonTime() + timer,
 		Message: msg,
 	};
+}
+
+export function ChatRoomActionMessage(msg, target = null, dictionary = []) {
+	if (!msg) return;
+	ServerSend("ChatRoomChat", {
+		Content: "BCX_PLAYER_CUSTOM_DIALOG",
+		Type: "Action",
+		Target: target ?? undefined,
+		Dictionary: [
+			{ Tag: "MISSING PLAYER DIALOG: BCX_PLAYER_CUSTOM_DIALOG", Text: msg },
+			...dictionary,
+		],
+	});
+}
+
+export function ChatRoomSendLocal(msg, timeout, sender) {
+	// Adds the message and scrolls down unless the user has scrolled up
+	const div = document.createElement("div");
+	div.setAttribute("class", "ChatMessage ChatMessageLocalMessage");
+	div.setAttribute("data-time", ChatRoomCurrentTime());
+	div.setAttribute("data-sender", `${sender ?? Player.MemberNumber ?? 0}`);
+	div.style.background = "#6e6eff54";
+	div.style.margin = "0.15em 0";
+
+	if (typeof msg === "string")
+		div.innerText = "[AES] " + msg;
+	else
+		div.appendChild("[AES] " + msg);
+
+	//if (timeout) BCX_setTimeout(() => div.remove(), timeout);
+
+	// Returns the focus on the chat box
+	const Refocus = document.activeElement?.id === "InputChat";
+	const ShouldScrollDown = ElementIsScrolledToEnd("TextAreaChatLog");
+	const ChatLog = document.getElementById("TextAreaChatLog");
+	if (ChatLog != null) {
+		ChatLog.appendChild(div);
+		if (ShouldScrollDown) ElementScrollToEnd("TextAreaChatLog");
+		if (Refocus) ElementFocus("InputChat");
+		return div;
+	}
+	return null;
 }
